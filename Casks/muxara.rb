0c31513f86ffbb81b9deb86cec04ca40240a4a0a
mani.kolbe@contino.io <mani.kolbe@contino.io>
2026-04-22 14:59:33 +0100

    Update Muxara to v0.1.0 with checksums and fix arch naming

--- Casks/muxara.rb
+++ Casks/muxara.rb
@@ -1,10 +1,9 @@
 cask "muxara" do
-  arch arm: "aarch64", intel: "x86_64"
+  arch arm: "aarch64", intel: "x64"
 
   version "0.1.0"
-  # SHA256 checksums will be updated by the release automation workflow
-  sha256 arm:   "PLACEHOLDER_ARM_SHA256",
-         intel: "PLACEHOLDER_INTEL_SHA256"
+  sha256 arm:   "b7e8a1fd844c65d4d65bffd9175d7ac0be994393bd6a1088d98d1752727903b5",
+         intel: "dfb1df6aed58e525428a0e078e827c216a829089a2c435665d1f79a772593fd5"
 
   url "https://github.com/muxara/muxara/releases/download/v#{version}/Muxara_#{version}_#{arch}.dmg",
       verified: "github.com/muxara/muxara/"
